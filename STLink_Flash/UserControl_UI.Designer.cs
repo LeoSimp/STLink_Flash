c38117bcd57eb86239fa5418a5d6215ea935a855
LeoSimp <34437467+LeoSimp@xitaopc@163.com>
2019-03-09 16:19:19 +0800

    Revert "V1.0.0.0"
    This reverts commit a66e4a73e9961f6beb48aa63ad7748be0d8710e5.

---
+++ STLink_Flash/UserControl_UI.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace STLink_Flash
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    partial class UserControl_UI
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_FileSet = new System.Windows.Forms.Button();
+            this.tb_FlashFile = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.rtb_STLink = new System.Windows.Forms.RichTextBox();
+            this.tb_FileNameFullString = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_RunBat = new System.Windows.Forms.Button();
+            this.cb_Debug = new System.Windows.Forms.CheckBox();
+            this.UC_Tittle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::STLink_Flash.Properties.Resources.ICO;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(417, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(3, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 15);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "File Name:";
+            // 
+            // btn_FileSet
+            // 
+            this.btn_FileSet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_FileSet.Location = new System.Drawing.Point(296, 114);
+            this.btn_FileSet.Name = "btn_FileSet";
+            this.btn_FileSet.Size = new System.Drawing.Size(124, 27);
+            this.btn_FileSet.TabIndex = 35;
+            this.btn_FileSet.Text = "烧录文件设置";
+            this.btn_FileSet.UseVisualStyleBackColor = true;
+            this.btn_FileSet.Click += new System.EventHandler(this.btn_FileSet_Click);
+            // 
+            // tb_FlashFile
+            // 
+            this.tb_FlashFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_FlashFile.Location = new System.Drawing.Point(6, 116);
+            this.tb_FlashFile.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_FlashFile.Name = "tb_FlashFile";
+            this.tb_FlashFile.ReadOnly = true;
+            this.tb_FlashFile.Size = new System.Drawing.Size(280, 25);
+            this.tb_FlashFile.TabIndex = 34;
+            this.tb_FlashFile.TextChanged += new System.EventHandler(this.tb_FlashFile_TextChanged);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label14.Location = new System.Drawing.Point(3, 189);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(160, 15);
+            this.label14.TabIndex = 33;
+            this.label14.Text = "STLink_Flash.bat:";
+            // 
+            // rtb_STLink
+            // 
+            this.rtb_STLink.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtb_STLink.Location = new System.Drawing.Point(3, 215);
+            this.rtb_STLink.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rtb_STLink.Name = "rtb_STLink";
+            this.rtb_STLink.ReadOnly = true;
+            this.rtb_STLink.Size = new System.Drawing.Size(417, 283);
+            this.rtb_STLink.TabIndex = 32;
+            this.rtb_STLink.Text = "";
+            // 
+            // tb_FileNameFullString
+            // 
+            this.tb_FileNameFullString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_FileNameFullString.BackColor = System.Drawing.Color.White;
+            this.tb_FileNameFullString.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_FileNameFullString.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.tb_FileNameFullString.Location = new System.Drawing.Point(6, 149);
+            this.tb_FileNameFullString.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_FileNameFullString.Multiline = true;
+            this.tb_FileNameFullString.Name = "tb_FileNameFullString";
+            this.tb_FileNameFullString.ReadOnly = true;
+            this.tb_FileNameFullString.Size = new System.Drawing.Size(414, 36);
+            this.tb_FileNameFullString.TabIndex = 37;
+            this.tb_FileNameFullString.Text = "File name full string";
+            // 
+            // btn_RunBat
+            // 
+            this.btn_RunBat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_RunBat.Enabled = false;
+            this.btn_RunBat.Location = new System.Drawing.Point(294, 184);
+            this.btn_RunBat.Name = "btn_RunBat";
+            this.btn_RunBat.Size = new System.Drawing.Size(124, 27);
+            this.btn_RunBat.TabIndex = 38;
+            this.btn_RunBat.Text = "Run BAT";
+            this.btn_RunBat.UseVisualStyleBackColor = true;
+            this.btn_RunBat.Click += new System.EventHandler(this.btn_RunBat_Click);
+            // 
+            // cb_Debug
+            // 
+            this.cb_Debug.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cb_Debug.AutoSize = true;
+            this.cb_Debug.Location = new System.Drawing.Point(254, 189);
+            this.cb_Debug.Name = "cb_Debug";
+            this.cb_Debug.Size = new System.Drawing.Size(37, 19);
+            this.cb_Debug.TabIndex = 39;
+            this.cb_Debug.Text = "D";
+            this.cb_Debug.UseVisualStyleBackColor = true;
+            this.cb_Debug.CheckedChanged += new System.EventHandler(this.cb_Debug_CheckedChanged);
+            // 
+            // UC_Tittle
+            // 
+            this.UC_Tittle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.UC_Tittle.BackColor = System.Drawing.Color.Transparent;
+            this.UC_Tittle.Font = new System.Drawing.Font("宋体", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.UC_Tittle.Location = new System.Drawing.Point(190, 0);
+            this.UC_Tittle.Name = "UC_Tittle";
+            this.UC_Tittle.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.UC_Tittle.Size = new System.Drawing.Size(227, 22);
+            this.UC_Tittle.TabIndex = 40;
+            this.UC_Tittle.Text = "Tittle";
+            this.UC_Tittle.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // UserControl_UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.UC_Tittle);
+            this.Controls.Add(this.cb_Debug);
+            this.Controls.Add(this.btn_RunBat);
+            this.Controls.Add(this.tb_FileNameFullString);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_FileSet);
+            this.Controls.Add(this.tb_FlashFile);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.rtb_STLink);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "UserControl_UI";
+            this.Size = new System.Drawing.Size(420, 500);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_FileSet;
+        private System.Windows.Forms.TextBox tb_FlashFile;
+        private System.Windows.Forms.Label label14;
+        internal System.Windows.Forms.RichTextBox rtb_STLink;
+        internal System.Windows.Forms.TextBox tb_FileNameFullString;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_RunBat;
+        private System.Windows.Forms.CheckBox cb_Debug;
+        private System.Windows.Forms.Label UC_Tittle;
+    }
+}
---
+++ STLink_Flash/UserControl_UI.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using System.Threading;
+
+namespace STLink_Flash
+{
+    public partial class UserControl_UI : UserControl
+    {
+        /// <summary>
+        /// 程序集名字
+        /// </summary>
+        public string MoudleConnString = Assembly.GetExecutingAssembly().GetName().Name;
+
+        /// <summary>
+        /// 程序集名字+后缀
+        /// </summary>
+        public string MoudleConnString_Ext = Path.GetFileName(Assembly.GetExecutingAssembly().Location);
+        /// <summary>
+        /// 
+        /// </summary>
+        public UserControl_UI()
+        {
+            InitializeComponent();
+            Assembly asm = Assembly.GetExecutingAssembly();
+            AssemblyFileVersionAttribute asmfver = (AssemblyFileVersionAttribute)Attribute.GetCustomAttribute(asm, typeof(AssemblyFileVersionAttribute));
+            UC_Tittle.Text = MoudleConnString + " Ver " + asmfver.Version;
+            UC_Tittle.Parent = pictureBox1;
+            mset_Load();         
+        }
+
+        internal void mset_Load()
+        {
+            try
+            {
+                string path = System.Windows.Forms.Application.StartupPath + @"\MoudleSettingFiles\";
+                tools.pcheck(path);
+                path += MoudleConnString + ".mset";
+                if (File.Exists(path))
+                {
+                    using (StreamReader sr = new StreamReader(path, System.Text.Encoding.GetEncoding("GB2312")))
+                    {
+                        tb_FlashFile.Text = sr.ReadLine();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                string DialogTittle = MoudleConnString_Ext;
+                MessageBox.Show("mset_Load初始化失败:" + ex.ToString(), DialogTittle);
+            }
+
+        }
+
+        //------------------以上为通用初始化区-----------------------------
+        //------------------以下为private内部函数和事件区-----------------------------
+
+        private void btn_FileSet_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog1.Filter = "烧录文件(*.bin,*.hex,*.srec,*.s19)|*.bin;*.hex;*.srec;*.s19|All files(*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tb_FlashFile.Text = openFileDialog1.FileName;
+                string path = System.Windows.Forms.Application.StartupPath + @"\MoudleSettingFiles\";
+                tools.pcheck(path);
+                path += MoudleConnString + ".mset";
+                using (StreamWriter sr = new StreamWriter(path, false, System.Text.Encoding.GetEncoding("GB2312")))
+                {
+                    string rst = "";                
+                    rst += tb_FlashFile.Text + "\r\n";
+                    sr.Write(rst);
+                }
+            }
+        }
+
+        private void tb_FlashFile_TextChanged(object sender, EventArgs e)
+        {
+            tb_FileNameFullString.Text = tb_FlashFile.Text;
+        }
+
+        private void cb_Debug_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb_Debug.Checked)
+            {
+                btn_RunBat.Enabled = true;
+            } else
+            {
+                btn_RunBat.Enabled = false;
+            }
+
+        }
+      
+        //Invoke回调函数
+        private delegate void InvokeCallback(Control c,string text);
+        private void UpdateText(Control c, string text)
+        {
+            if (c.InvokeRequired)//如果调用控件的线程和创建控件的线程不是同一个则为True
+            {
+                while (!c.IsHandleCreated)
+                {
+                    //解决窗体关闭时出现“访问已释放句柄“的异常
+                    if (c.Disposing || c.IsDisposed)
+                        return;
+                }
+                InvokeCallback d = new InvokeCallback(UpdateText); //回调
+                c.Invoke(d, new object[] { c,text });
+            }
+            else
+            {
+                c.Text += text + "\r\n";
+                if (c.GetType() == typeof(RichTextBox))
+                {
+                    RichTextBox rtb = (RichTextBox)c;
+                    rtb.Select(c.Text.Length, 0);
+                    rtb.ScrollToCaret();
+                }
+           
+            }
+        } 
+
+        private int StartProcess(string filename, string argument, int timeout_ms)
+        {
+            argument = argument.Trim();
+            Process process = new Process();//创建进程对象    
+            ProcessStartInfo startInfo = new ProcessStartInfo(filename, argument); // 括号里是(程序名,参数)
+            process.StartInfo = startInfo;
+            process.StartInfo.UseShellExecute = false;    //是否使用操作系统的shell启动,shell启动就无法获取StandardOutput
+            startInfo.RedirectStandardInput = true;      //接受来自调用程序的输入     
+            startInfo.RedirectStandardOutput = true;     //由调用程序获取输出信息
+            startInfo.CreateNoWindow = true;             //不显示调用程序的窗口，true=不显示
+            process.EnableRaisingEvents = true;
+            process.ErrorDataReceived += new DataReceivedEventHandler(ErrorHandler);
+            process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+            try
+            {
+                process.Start();
+                process.BeginOutputReadLine(); //异步
+                //string output = process.StandardOutput.ReadToEnd(); //同步,异步和同步不可同时执行
+                /*
+                string line = process.StandardOutput.ReadLine();//同步,每次读取一行
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    rtb_STLink.AppendText(line + "\r\n");
+                    line = process.StandardOutput.ReadLine();
+                }                           
+                */
+                int i = 0;
+                while (!process.HasExited)
+                {
+                    ////等待程序执行完退出进程,并让窗口实时更新
+                    Thread.Sleep(100);
+                    i++;
+                    Application.DoEvents();
+                    if (i >= timeout_ms / 100)
+                    {
+                        process.Kill();
+                        MessageBox.Show("等待超时，强制关闭调用Process进程，Timout=" + timeout_ms + "ms");
+                        return -1;
+                    }
+                }
+                return process.ExitCode;
+
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("需要提升")) { MessageBox.Show(ex.Message + Environment.NewLine + "需要以管理员身份执行程序"); }
+                else
+                {
+                    MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace);
+                }
+
+                return -1;
+            }
+
+        }
+
+        private int StartProcess(string filename, string argument, int timeout_ms,out string output)
+        {
+            argument = argument.Trim();
+            Process process = new Process();//创建进程对象    
+            ProcessStartInfo startInfo = new ProcessStartInfo(filename, argument); // 括号里是(程序名,参数)
+            process.StartInfo = startInfo;
+            process.StartInfo.UseShellExecute = false;    //是否使用操作系统的shell启动,shell启动就无法获取StandardOutput
+            startInfo.RedirectStandardInput = true;      //接受来自调用程序的输入     
+            startInfo.RedirectStandardOutput = true;     //由调用程序获取输出信息
+            startInfo.CreateNoWindow = true;             //不显示调用程序的窗口，true=不显示
+            process.EnableRaisingEvents = true;
+            process.ErrorDataReceived += new DataReceivedEventHandler(ErrorHandler);
+            process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+            output = null;
+            try
+            {
+                process.Start();
+                //process.BeginOutputReadLine(); //异步
+               output = process.StandardOutput.ReadToEnd(); //同步,异步和同步不可同时执行
+                /*
+                string line = process.StandardOutput.ReadLine();//同步,每次读取一行
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    rtb_STLink.AppendText(line + "\r\n");
+                    line = process.StandardOutput.ReadLine();
+                }                           
+                */
+                int i = 0;
+                while (!process.HasExited)
+                {
+                    ////等待程序执行完退出进程,并让窗口实时更新
+                    Thread.Sleep(100);
+                    i++;
+                    Application.DoEvents();
+                    if (i >= timeout_ms / 100)
+                    {
+                        process.Kill();
+                        MessageBox.Show("等待超时，强制关闭调用Process进程，Timout=" + timeout_ms + "ms");
+                        return -1;
+                    }
+                }
+                return process.ExitCode;
+
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("需要提升")) { MessageBox.Show(ex.Message + Environment.NewLine + "需要以管理员身份执行程序"); }
+                else
+                {
+                    MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace);
+                }
+
+                return -1;
+            }
+
+        }
+
+        private void OutputHandler(object sender, DataReceivedEventArgs e)
+        {
+            //动态读取,以下2个委托任选一个，第一个最简单，但无法设置自动下拉文本框
+            //rtb_STLink.BeginInvoke(new MethodInvoker(() => rtb_STLink.Text += e.Data + "\r\n"));
+            UpdateText(rtb_STLink, e.Data);
+            WriteLog(System.Windows.Forms.Application.StartupPath + @"\" + MoudleConnString + ".log", e.Data);
+        }
+
+        private void ErrorHandler(object sender, DataReceivedEventArgs e)
+        {
+            //MessageBox.Show(e.Data);
+        }
+
+
+        //常用Public接受调用的接口函数，需搭配以下调试按钮，来演示调用
+        private void btn_RunBat_Click(object sender, EventArgs e)
+        {
+            btn_RunBat.Enabled = false;
+            cb_Debug.Enabled = false;
+            string RunLog = null;
+            RunBatFLASH(out RunLog);
+            Console.WriteLine(RunLog);
+            cb_Debug.Enabled = true;
+            cb_Debug.Checked = false;
+        }
+
+        private void WriteLog(string FullLogName, string Message)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(FullLogName, true, Encoding.Default, 512); // 创建写入流
+                sw.WriteLine(Message);
+                sw.Close(); //关闭文件
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace);
+                return;
+            }
+
+        }
+
+        //------------------以下为Public接受调用的接口区-----------------------------
+
+        /// <summary>
+        /// 获取批处理执行的Log及执行返回的ExitCode(string类型)
+        /// </summary>        
+        /// <returns>返回ExitCode(string类型)</returns>
+        public string RunBatFLASH(out string RunLog)
+        {
+            rtb_STLink.Text = "";
+            rtb_STLink.ForeColor = Color.Black;
+            //RunLog = System.Windows.Forms.Application.StartupPath + @"\" + MoudleConnString + ".log";
+            //if (File.Exists(RunLog)) File.Delete(RunLog);
+            string output = null;
+            string rst=StartProcess(Environment.CurrentDirectory + @"\STLINK_FLASH.bat", "\"" + tb_FileNameFullString.Text + "\"", 120000,out output).ToString();
+            rtb_STLink.Text = output;
+            if (rst == "0")
+            {
+                rtb_STLink.ForeColor = Color.Green;
+            }
+            else
+            {
+                rtb_STLink.ForeColor = Color.Red;
+            }
+            rtb_STLink.Update();
+            RunLog = output;
+            return rst;
+
+        }
+  
+    }
+
+
+  
+}
